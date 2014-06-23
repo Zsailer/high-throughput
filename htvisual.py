020fdc71b36ef4a9fdb6ef817b5ea0b066d6b20a
Zachary Sailer <zachsailer@gmail.com>
2014-06-23 11:02:35 -0700

    Add doc strings to viz module

--- htvisual.py
+++ htvisual.py
@@ -11,8 +11,31 @@ class HighThroughputVisualization(object):
         self.y_label_size = 16
         
     def plot_sequence_freq(self, full0, full1, freq0, freq1, ref_string=None, normalization=1, labels_on=True):
-        """ Plot a bar graph of all sequences and their ratio of frequencies """
+        """ 
+        Plot a bar graph of all sequences and their ratio of frequencies before/after screening 
         
+        Parameters:
+        ----------
+        full0: dict
+            Full set of sequencing data before screening, sequence-count (key, value) pair
+        full1: dict 
+            Full set of sequencing data after screening, sequence-count (key, value) pair
+        freq0: dict
+            Sampled set of sequencing data before screening, sequence-count (key, value) pair
+        freq1: dict
+            Sampled set of sequencing data after screening, sequence-count (key, value) pair
+        ref_string: str
+            Reference string to normalize the data, must have length(sequence)
+        normalization: anything
+            Determines whether to do a normalization
+        labels_on: bool (default True)
+            Labels bars with the corresponding sequence
+            
+        Returns:
+        -------
+        seq_freq_plot: matplotlib bar plot
+        """
+
         seq_labels = freq0.keys()
         x_marks = np.arange(len(freq0))
         
@@ -20,6 +43,8 @@ class HighThroughputVisualization(object):
             if ref_string is None:
                 # Dirty hack to get string of ones with len(sequences)
                 ref_string = "".join([str(int(one)) for one in np.ones(len(seq_labels[0]))])
+            elif len(ref_string) is not len(seq_labels[0]):
+                raise NameError('ref_string must have length: ' + str(len(seq_labels[0])))
             normalization = float(freq1[ref_string]*full0[ref_string])/(freq0[ref_string]*full1[ref_string])
             
         ratio = list()
@@ -39,7 +64,7 @@ class HighThroughputVisualization(object):
         return seq_freq_plot
         
     def plot_kmer_interactions(self, kmer_interactions):
-        """ Plot all k order interactions for the system """
+        """ Plot a bar chart of all k-order interactions for the system """
         
         seq_labels = kmer_interactions.keys()
         seq_values = kmer_interactions.values()
